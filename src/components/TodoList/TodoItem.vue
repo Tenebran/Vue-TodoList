f6c73c9479bf05a8280d3a2c878201d3034a6e18
Sergej Garkusha <Tenebrann@googlemail.com>
2023-05-20 18:58:28 +0200

    Update Todolist

--- src/components/TodoList/TodoItem.vue
+++ src/components/TodoList/TodoItem.vue
@@ -3,20 +3,29 @@
     class="todo"
     :class="{
       'completed': props.todo.isComplited,
+      'editing': isEditing
     }"
   >
     <div class="view">
       <input class="toggle" type="checkbox" @input="$emit('toggleState', props.todo.id)" 
       :checked="props.todo.isComplited"
       />
-      <label>{{ props.todo.title }} </label>
+      <label @dblclick="isEditing = true">{{ props.todo.title }} </label>
       <button class="destroy"></button>
     </div>
-    <input class="edit" type="text" />
+    <input class="edit" type="text" :value="props.todo.title"  @keydown.enter="stopEditing"/>
   </li>
 </template>
 <script setup>
+import { ref } from 'vue';
+
 const props = defineProps(['todo']);
-console.log(props.todo.isComplited);
+const isEditing = ref(false)
+
+const emit = defineEmits()
+function stopEditing(event) {
+  isEditing.value = false
+  emit('change-title',  {id:  props.todo.id, newTitle: event.target.value} )
+}
 </script>
 <style></style>
